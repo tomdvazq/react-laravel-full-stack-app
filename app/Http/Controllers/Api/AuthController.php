6450a6ebd67953518fe5083dcac08e68f8a75bde
timi <tomdvazq@gmail.com>
2023-08-04 20:32:13 -0300

    add public functions to login, signup and logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignUpRequest;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+
+    public function login(LoginRequest $request) {
+        
+    }
+
+    public function signup(SignUpRequest $request) {
+
+    }
+
+    public function logout(Logout $request) {
+
+    }
 }
