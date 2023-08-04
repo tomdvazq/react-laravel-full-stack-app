f4c331ec900a6481f4d5935d19ad99ab4bf71d30
timi <tomdvazq@gmail.com>
2023-08-04 20:06:38 -0300

    edit signup

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,7 +1,14 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    // Lógica
+    
+    const nameRef = useRef();
+    const surnameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -19,6 +26,7 @@ const SignUp = () => {
                     <input type="text" placeholder="Surname" />
                     <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Confirm your password" />
                     <button className="btn btn-block">Login</button>
                     <p style={{textAlign: 'center', marginTop: 25}}>
                         Have an account? <Link to="/login">Login</Link>
