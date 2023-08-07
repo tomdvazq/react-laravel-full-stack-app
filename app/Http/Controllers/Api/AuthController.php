5a762fd34398db60cce5dc9ff09c6d5bb128d438
timi <tomdvazq@gmail.com>
2023-08-07 15:47:42 -0300

    add rules to signup

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignUpRequest;
+use App\Models\User;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -29,15 +31,26 @@ class AuthController extends Controller
     public function login(LoginRequest $request)
     {
         $credentials = $request->validate();
-        if(!Auth::attempt($credentials)) {
+
+        if (!Auth::attempt($credentials)) {
             return response([
                 'message' => 'Provided email address or password is incorrect'
             ]);
         }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken(name: 'main')->plainTextToken;
+
+        return response(compact('user', 'token'));
     }
 
 
     public function logout(Logout $request)
     {
+        /** @var User $user */
+
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
     }
 }
--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class SignUpRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class SignUpRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,16 @@ class SignUpRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:55',
+            'email' => 'required|email|unique:users,email',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)
+                ->letters()
+                ->symbols()
+                ->numbers()
+            ]
         ];
     }
 }
